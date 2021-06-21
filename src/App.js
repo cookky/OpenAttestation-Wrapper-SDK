23ccfb497b3f547e94681ab9f768fb2b470673a9
cookky <cookky.developwe@gmail.com>
2021-06-21 17:21:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import document from './document.json'
+import { wrapDocument } from '@govtechsg/open-attestation'
+import util from 'util'
 
 function App() {
+  const wrappedDocument = wrapDocument(document);
+
+  console.log(util.inspect(wrappedDocument, { showHidden: false, depth: null }));
   return (
     <div className="App">
       <header className="App-header">
